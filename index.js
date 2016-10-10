afa1e39613b726f3a42afb3d2538e8dca81c1985
KDepp <kdepp.cd@gmail.com>
2016-10-10 15:56:28 +0800

    add: backend log

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var MongoStore = require('connect-mongo')(session);
 var flash = require('connect-flash');
+var FileStreamRotator = require('file-stream-rotator');
+var morgan = require('morgan');
 
 var u = require('./common/utils');
 var router = require('./routers');
 var passport = require('./common/passport');
 var io = require('./websocket/io');
 
+var logDirectory = path.join(__dirname, 'log');
 var app = express();
 var sessionMiddleWare = session({
   secret: 'hive6 coming',
@@ -19,6 +24,8 @@ var sessionMiddleWare = session({
     url: 'mongodb://localhost/hive6'
   })
 });
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 io({ middlewares: [sessionMiddleWare] });
 
@@ -35,6 +42,14 @@ app.use(function (err, req, res, next) {
   res.status(500);
   res.send('Something Wrong: ' + err.stack);
 });
+app.use(morgan('combined', {
+  stream: FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: path.join(logDirectory, 'access-%DATE%.log'),
+    frequency: 'daily',
+    verbose: false
+  })
+}));
 
 u.setRouter(app, router);
 
