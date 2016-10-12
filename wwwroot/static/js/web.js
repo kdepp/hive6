6e3852c92b059030748fdb8c6df1e27119ef9020
KDepp <kdepp.cd@gmail.com>
2016-10-12 16:10:38 +0800

    add: browser feature test

--- wwwroot/static/js/web.js
+++ wwwroot/static/js/web.js
@@ -1,9 +1,21 @@
 /* global globalPlayerTypes globalGameId */
 
 require('es6-shim');
+require('./common/custom_modenizr');
+
 var gameFactory = require('./hive/web_game');
 
-var game = gameFactory({
+if (!window.Modernizr.canvas || !window.Modernizr.websockets) {
+  alert([
+    '您的浏览器过时了，可以考虑使用以下浏览器访问昆虫棋网',
+    '- Chrome 49+',
+    '- Firefox 47+',
+    '- Safari 9.1+',
+    '- IE 11+',
+  ].join('\n'));
+}
+
+gameFactory({
   document: document,
   $boardContainer: document.getElementById('canvas_container'),
   $replayContainer: document.getElementById('replay_container'),
@@ -14,5 +26,3 @@ var game = gameFactory({
   playertypes: globalPlayerTypes,
   gameId: globalGameId
 });
-
-console.log(game);
